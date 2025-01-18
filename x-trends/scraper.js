b55aa251aab00e016538a695854dc743b48777c7
MD IMRAN ALAM <97190376+immo7932@users.noreply.github.com>
2025-01-18 17:35:32 +0530

    Update scraper.js

--- x-trends/scraper.js
+++ x-trends/scraper.js
@@ -4,8 +4,8 @@ const { v4: uuidv4 } = require('uuid');
 const fetch = require('node-fetch');
 
 
-const X_USERNAME = 'immo7932';           
-const X_PASSWORD = 'Imranalam@123';      
+const X_USERNAME = 'USERNAME';           
+const X_PASSWORD = 'PASSWORD';      
 const CHROME_DRIVER_PATH = 'C:\\Windows\\chromedriver.exe';
 
 // ProxyMesh Configuration
